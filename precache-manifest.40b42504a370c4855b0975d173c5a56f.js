0ba40b06eead5d7eb2064357524decf644c9856a
thinhtn <thinhtn@users.noreply.github.com>
2021-08-13 13:56:17 +0000

    deploy: 8b42dd38cdb833f5dd69a2ee70936695352c13e7

---
+++ precache-manifest.40b42504a370c4855b0975d173c5a56f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16dc69475b50f22e748013ad92e53e85",
+    "url": "https://thinhtn.github.io/task-management-frontend/index.html"
+  },
+  {
+    "revision": "86332a74b5245a651c53",
+    "url": "https://thinhtn.github.io/task-management-frontend/static/css/main.5ac26d30.chunk.css"
+  },
+  {
+    "revision": "2c486d1477b6302004cb",
+    "url": "https://thinhtn.github.io/task-management-frontend/static/js/2.60611d7b.chunk.js"
+  },
+  {
+    "revision": "86332a74b5245a651c53",
+    "url": "https://thinhtn.github.io/task-management-frontend/static/js/main.1d3c1102.chunk.js"
+  },
+  {
+    "revision": "3821cbf5978b99dda503",
+    "url": "https://thinhtn.github.io/task-management-frontend/static/js/runtime~main.7fbfccaf.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://arielweinberger.github.io/task-management-frontend/precache-manifest.0a88baa7b4ebb92ea89c64cd3e3f4832.js"
+  "https://thinhtn.github.io/task-management-frontend/precache-manifest.40b42504a370c4855b0975d173c5a56f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://arielweinberger.github.io/task-management-frontend/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://thinhtn.github.io/task-management-frontend/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
